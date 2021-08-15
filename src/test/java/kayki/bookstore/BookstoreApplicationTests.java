89addcc1ca68daf59f0ca3fb3767bc9d5340e886
Kayki <grcdjkayki@gmail.com>
2021-08-15 14:10:09 -0300

    Project start

---
+++ src/test/java/kayki/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kayki.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
